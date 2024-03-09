e0484d9b47a6b234291b73ecc2b2b3018a257cc7
Denis <damned_wolf@mail.ru>
2024-03-09 19:09:13 +0300

    HW_1

---
+++ HW_1/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.server.ServerWindow;
+import org.example.client.ClientGUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ServerWindow serverWindow = new ServerWindow();
+        new ClientGUI(serverWindow);
+    }
+}
